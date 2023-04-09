f32fada87c1c97e85a0289206d6636a10052b657
ghisl77 <129007773+ghisl77@users.noreply.github.com>
2023-04-09 20:02:41 +0200

    Add files via upload

---
+++ graphe/GrapheLArcs.java
@@ -0,0 +1,165 @@
+package graphe;
+import arc.Arc;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+public class GrapheLArcs implements IGraphe{
+    private ArrayList<Arc> LArcs;
+    public GrapheLArcs(){
+        LArcs = new  ArrayList<Arc>();
+    }
+    public GrapheLArcs(String description_graphe){
+        this();
+        String[] s =  description_graphe.split(",");
+        /*for (int i = 0; i < s.length; i++) {
+            LArcs.add(new Arc(s[i].substring(0,0),s[i].substring(2,2),Integer.parseInt(s[i].substring(4,4))));
+        }*/
+        this.peupler(description_graphe);
+    }
+    @Override
+    public void ajouterSommet(String noeud){
+        if(!this.contientSommet(noeud)){
+            LArcs.add(new Arc(noeud,"",-1));
+        }
+        //else
+            //throw new IllegalArgumentException("le noeud existe deja");
+    }
+    @Override
+    public void ajouterArc(String source, String destination, Integer valeur)throws IllegalArgumentException{
+        if(valeur<0){
+            throw new IllegalArgumentException("valuation négative");
+        }
+        Arc arc = new Arc(source,destination,valeur);
+        if(!this.contientArc(source,destination)){
+            LArcs.add(arc);
+            for (int i = 0; i < LArcs.size(); i++) {
+                if(LArcs.get(i).getSource().compareTo(source)==0 && LArcs.get(i).getValuation()==-1){
+                    LArcs.remove(i);
+                }
+            }
+        }
+        else
+            throw new IllegalArgumentException("déja présent");
+
+    }
+    @Override
+    public void oterSommet(String noeud){
+        if(this.contientSommet(noeud) ){
+            for (int i = 0; i < LArcs.size(); i++) {
+                if (LArcs.get(i).getSource() == noeud ) {
+                    LArcs.add(i,new Arc("",LArcs.get(i).getDestination(),0));
+                }
+                if(LArcs.get(i).getDestination() == noeud){
+                    LArcs.add(i,new Arc("",LArcs.get(i).getSource(),0));
+                }
+            }
+        }
+    }
+    @Override
+    public void oterArc(String source, String destination)throws IllegalArgumentException{
+        if(this.contientArc(source,destination)){
+            for (int i = 0; i < LArcs.size(); i++) {
+                if(LArcs.get(i).getSource()==source && LArcs.get(i).getDestination()==destination){
+                    LArcs.remove(i);
+                }
+            }
+        }
+        else
+            throw new IllegalArgumentException("n'existe pas");
+    }
+    @Override
+    public List<String> getSommets(){
+        int cmp=0;
+        ArrayList<String> som = new ArrayList<String>();
+        for (int i = 0; i < LArcs.size(); i++) {
+            for (int j = 0; j < som.size(); j++) {
+                if(LArcs.get(i).getSource().compareTo(som.get(j))==0){
+                    cmp=1;
+                }
+            }
+            if(cmp==0)
+                som.add(LArcs.get(i).getSource());
+            cmp=0;
+        }
+
+        return som;
+    } // pas forcement triee
+    @Override
+    public List<String> getSucc(String sommet){
+        int cmp=0;
+        ArrayList<String> succ = new ArrayList<String>();
+            for (int i = 0; i < LArcs.size(); i++) {
+                if(LArcs.get(i).getSource().compareTo(sommet)==0 ){
+                    for (int j = 0; j < succ.size(); j++) {
+                        if(succ.get(j).compareTo(LArcs.get(i).getDestination())==0){
+                            cmp=1;
+                        }
+                    }
+                    if(cmp==0 && LArcs.get(i).getDestination().compareTo("")!=0)
+                        succ.add(LArcs.get(i).getDestination());
+                    cmp=0;
+                }
+            }
+        return succ;
+    } // pas forcement triee
+    @Override
+    public int getValuation(String src, String dest){
+        for (int i = 0; i < LArcs.size(); i++) {
+            if (LArcs.get(i).getSource().compareTo(src)==0 && LArcs.get(i).getDestination().compareTo(dest) == 0) {
+                return LArcs.get(i).getValuation();
+            }
+        }
+        return -1;
+    } // les sommets doivent exister, -1 si pas d'arc
+    @Override
+    public boolean contientSommet(String sommet){
+        List<String> clone=this.getSommets();
+        for (int i = 0; i <clone.size(); i++) {
+            if(clone.get(i).compareTo(sommet)==0){
+                return true;
+            }
+        }
+        return false;
+    }
+    @Override
+    public boolean contientArc(String src, String dest){
+        for (int i = 0; i < LArcs.size(); i++) {
+            if(LArcs.get(i).getSource().compareTo(src)==0 && LArcs.get(i).getDestination().compareTo(dest)==0){
+                return true;
+            }
+        }
+        return false;
+    }
+    @Override
+    public String toString(){
+        Arc supstitution ;
+        ArrayList<Arc> arc2 = new ArrayList<>();//il faut que je trie dans l'ordre croissant les LArcs
+        for (int i = 0; i < LArcs.size(); i++) {
+            arc2.add(LArcs.get(i));
+        }
+        for (int j = 0; j < arc2.size()*400; j++) {
+            for (int i = 1; i < arc2.size(); i++) {
+                if(arc2.get(i).getSource().compareTo(arc2.get(i - 1).getSource()) == 0){
+                   if(arc2.get(i).getDestination().compareTo(arc2.get(i - 1).getDestination()) < 0) {
+                       supstitution = new Arc(arc2.get(i).getSource(), arc2.get(i).getDestination(), arc2.get(i).getValuation());
+
+                       arc2.add(i - 1, supstitution);
+                       arc2.remove(i + 1);
+                    }
+                }
+                if ((arc2.get(i).getSource().compareTo(arc2.get(i - 1).getSource()) < 0) ){
+                    supstitution = new Arc(arc2.get(i).getSource(), arc2.get(i).getDestination(), arc2.get(i).getValuation());
+                    arc2.add(i - 1, supstitution);
+                    arc2.remove(i + 1);
+                }
+            }
+        }
+        StringBuilder s = new StringBuilder();
+        for (int i = 0; i < arc2.size(); i++) {
+            s.append(arc2.get(i).toString());
+        }
+        return s.substring(0,s.length()-2).toString();
+    }
+}
