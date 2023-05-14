ad0225d97eb891b90dbe7d394de2c46fa378e16a
MrLojnat <krajakrajina@gmail.com>
2023-05-14 15:25:31 +0200

    changement des == et != entre strings par des .equals()

--- dijkstra/dijkstra.java
+++ dijkstra/dijkstra.java
@@ -1,34 +1,33 @@
-package Djikstra;
+package dijkstra;
 
 import graphe.IGrapheConst;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class Djikstra {
+public class dijkstra {
     public void djikstra(IGrapheConst graphe, String source, String destination){
         String[][] tab = new String [graphe.getSommets().size()][1];
 
         for(int i = 0; i < tab.length; ++i){
             tab[i][0] = graphe.getSommets().get(i);
-            if(tab[i][0] == source) tab[i][1] = "0";
+            if(tab[i][0].equals(source)) tab[i][1] = "0";
         }
 
         String position = source;
 
-        while (position != destination){
+        while (!position.equals(destination)){
             List<String> succ = graphe.getSucc(position);
 
-            if(position == source){
+            if(position.equals(source)){
                 for(int i = 0; i < succ.size(); ++i){
-                    if(tab[i][0] == succ.get(i)){
+                    if(tab[i][0].equals(succ.get(i))){
                         tab[i][1] = String.valueOf(graphe.getValuation(position, succ.get(i)));
                     }
                 }
                 position = minValue(tab);
             }
-            else{
+            else {
                 
             }
 
